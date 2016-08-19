1cd991a47a546afab65038a4ee753dc70278f6ba
Luke <hello@lukeonthebrightside.com>
2016-08-20 07:13:27 +0800

    unit 9 complete

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+alert('Hello, friends.');
+
+if (cardOne === cardTwo ){
+  alert ("You found a match!");
+}
